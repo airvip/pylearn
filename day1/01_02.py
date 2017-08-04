8e9236ffb4201e110b076762de008da248f2e1ad
airvip <sdqhwzb@163.com>
2017-08-04 09:26:12 +0800

    yaya

--- day1/01_02.py
+++ day1/01_02.py
@@ -2,3 +2,11 @@
 # -*- coding:utf-8 -*-
 # Created by airvip on 2017/8/3 19:10.
 
+name = "air vip"
+name2 = name
+print("my name is ",name,name2)
+
+name = "wzb"
+
+gf_of_airvip = "yaya"
+print(name,name2)
