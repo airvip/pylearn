59ad0a037465cb19f316d0d1b07e0557ebe056b2
airvip <sdqhwzb@163.com>
2017-08-04 10:09:35 +0800

    learn

--- day1/01_03.py
+++ day1/01_03.py
@@ -2,5 +2,45 @@
 # -*- coding:utf-8 -*-
 # Created by airvip on 2017/8/4 9:28.
 
+msg = """
 name = "你好 ,世界"
 print(name)
+"""
+
+print(msg)
+
+name = input("What is your name ?")
+password = input("enter your password ?")
+print("my name is ",name)
+print("password is ",password)
+
+
+name = input("your name:")
+age = int(input("your age:"))
+job = input("your job:")
+
+info = """
+--------- info of %s ---------
+name:%s
+age:%d
+job:%s
+"""%(name,name,age,job)
+
+
+print(info )
+
+info2 = """
+--------- info of {_name} ---------
+name:{_name}
+age:{age}
+job:{job}
+""".format(_name=name,age=age,job=job)
+print(info2)
+
+info3 = """
+--------- info of {0} ---------
+name:{0}
+age:{1}
+job:{2}
+""".format(name,age,job)
+print(info3)
---
+++ day1/01_04.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2017/8/4 9:59.
+
+import getpass
+
+username = input("username:")
+password = getpass.getpass("password:")
+
+print(username,password)
