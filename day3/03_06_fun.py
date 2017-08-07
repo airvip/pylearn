95068f8ca9d173c015d0a19e2f2135adb463936e
airvip <sdqhwzb@163.com>
2017-08-07 18:48:47 +0800

    learn file

---
+++ day3/03_06_fun.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2017/8/7 17:52.
+
+#函数
+def func1():
+    """testing"""
+    print("in the func1")
+    return 0
+
+#过程
+def func2():
+    """testing2"""
+    print("in the func2")
+
+
+x1=func1()
+x2=func2()
+
+print("func1 return is %s" %x1)
+print("func2 return is %s" %x2)
