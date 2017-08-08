d2e3d04873378d62a30ef2c41b8d3c7f7723f6f1
airvip <sdqhwzb@163.com>
2017-08-08 18:27:24 +0800

    learn function

--- day3/03_09_func.py
+++ day3/03_09_func.py
@@ -17,9 +17,34 @@ test(1,z=3,y=2)
 def test1(x,y=2):
     print(x)
     print(y)
-'''
-def test2(*arg):
-    print(arg)
 
 
+def test2(*args):
+    print(args)
 test2(1,2,3,4,5)
+test2(*[1,2,3,4,5])
+
+def test3(x,*args):
+    print(x)
+    print(args)
+test3(1,2,3,4,5)
+test3(*[1,2,3,4,5])
+
+def test4(**kwargs):
+    print(kwargs)
+test4(name="airvip",age="21",sex="F")
+test4(**{"name":"airvip","age":"21","sex":"F"})
+
+def test5(name,**kwargs):
+    print(name)
+    print(kwargs)
+test5("airvip",age="21",sex="F")
+test5("airvip",**{"age":"21","sex":"F"})
+'''
+def test6(name,age=12,**kwargs):
+    print(name)
+    print(age)
+    print(kwargs)
+test6("airvip",love="pp",sex="F")
+test6("airvip",love="pp",sex="F",age=18)
+test6("airvip",**{"age":"21","sex":"F"})
