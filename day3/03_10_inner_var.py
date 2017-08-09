611b64da954f36f58107ebd6c13ca58020133afa
airvip <sdqhwzb@163.com>
2017-08-09 09:58:26 +0800

    learn variable

---
+++ day3/03_10_inner_var.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2017/8/9 9:42.
+
+#下面代码谁用开除谁
+
+'''
+school = "12306"
+
+def change_name(name):
+    global school
+    school = "17951"
+    print(name)
+    name = "AIR VIP"
+    print(name)
+
+name = "airvip"
+change_name(name)
+print(name)
+print(school)
+'''
+
+def change_variable():
+    global lala
+    lala = "17951"
+
+change_variable()
+print(lala)
