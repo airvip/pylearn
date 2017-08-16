edbbceef46a81f251c8b7a328cac55d678d0a8d2
airvip <sdqhwzb@163.com>
2017-08-16 17:13:25 +0800

    multiple inheritance

---
+++ week6/03_multipleinheritance.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2017/8/16 16:47.
+
+#Python2经典类(继承深度优先继承搜索法) 新式类（继承广度优先继承搜索法）
+#Python3继承广度优先继承搜索法
+class A:
+    def __init__(self):
+        print('A')
+
+class B(A):
+    pass
+    # def __init__(self):
+    #     print('B')
+
+class C(A):
+    pass
+    # def __init__(self):
+    #     print('C')
+
+class D(B,C):
+    pass
+    # def __init__(self):
+    #     print('C')
+
+D()
--- week6/extend/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# Created by airvip on 2017/8/16 11:32.
-
