67f46c0d750fb58fd85c26f1cfe0173328fe822e
airvip <sdqhwzb@163.com>
2017-08-16 16:41:36 +0800

    learn class

--- week6/extend/people.py
+++ week6/extend/people.py
@@ -2,11 +2,14 @@
 # -*- coding:utf-8 -*-
 # Created by airvip on 2017/8/16 11:32.
 
-class People:
-
+#经典类
+#class People:
+#新式类
+class People(object):
     def __init__(self,name,age):
         self.name = name
         self.age = age
+        self.friends = []
 
     def eat(self):
         print("%s is eating"%self.name)
@@ -17,7 +20,22 @@ class People:
     def sleep(self):
         print("%s is sleeping"%self.name)
 
-class Man(People):
+
+class Relation(object):
+
+    def make_friends(self,obj):
+        print("%s is making frends with %s" %(self.name,obj.name))
+        self.friends.append(obj)
+
+
+
+class Man(People,Relation):
+
+    def __init__(self,name,age,money):
+        #People.__init__(self,name,age)
+        super(Man,self).__init__(name,age)#新式类写法
+        self.money = money
+        print("%s borning has %s money"%(self.name,self.money))
 
     def piao(self):
         print("%s is piaoing ...20s... done" %self.name)
@@ -26,15 +44,22 @@ class Man(People):
         People.sleep(self)
         print("man is sleeping")
 
-class Women(People):
+class Women(People,Relation):
 
     def get_birth(self):
         print("%s is born a baby..."%self.name)
 
-man1 = Man("airvip",22)
+
+man1 = Man("airvip",22,555)
 man1.eat()
 man1.piao()
 man1.sleep()
 
 women1 = Women("yaya",21)
 women1.get_birth()
+
+
+man1.make_friends(women1)
+
+women1.name = "yueer"
+print(man1.friends[0].name)
