af16e0455b9f87aa2ef70de363767383ff93b610
airvip <sdqhwzb@163.com>
2017-08-19 17:27:47 +0800

    socket learn

--- week7/09_socket_client.py
+++ week7/09_socket_client.py
@@ -3,5 +3,12 @@
 # Created by airvip on 2017/8/18 18:03.
 
 import socket
+#客户端
+client = socket.socket()#声明socket类型，同时生成socket连接对象
+client.connect(('localhost',6969))
 
-socket.socket()
+client.send(b"Hello World!")
+data = client.recv(1024)
+print("recive data",data)
+
+client.close()
