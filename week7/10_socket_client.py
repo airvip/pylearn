edbe3f0094cc92607e125765c509ced0c5c10ad2
airvip <sdqhwzb@163.com>
2017-08-20 23:23:22 +0800

    socket

---
+++ week7/10_socket_client.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2017/8/18 18:03.
+
+import socket
+#客户端
+client = socket.socket()#声明socket类型，同时生成socket连接对象
+client.connect(('localhost',6969))
+
+# client.send(b"Hello World!")
+while True:
+    msg = input(">>:").strip()
+    if len(msg) == 0:
+        continue
+    client.send(msg.encode("utf-8"))
+    data = client.recv(1024)
+    print("recive data：",data.decode())
+
+client.close()
---
+++ week7/10_socket_server.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2017/8/19 17:12.
+
+import socket
+#服务器端
+server = socket.socket()
+server.bind(('localhost',6969))#绑定要监听的端口
+server.listen()#监听
+
+print("i wait connect")
+conn,addr = server.accept()#等待连接
+print(conn,addr)
+print("connect arrived")
+while True:
+    data = conn.recv(1024)
+    print("recive:",data)
+    conn.send(data.upper())
+
+server.close()
