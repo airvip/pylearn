b1720fd8a3a06c03ba87209fa00215bd95849803
airvip <sdqhwzb@163.com>
2017-08-20 23:47:54 +0800

    socket server

--- week7/10_socket_server.py
+++ week7/10_socket_server.py
@@ -7,15 +7,23 @@ import socket
 #服务器端
 server = socket.socket()
 server.bind(('localhost',6969))#绑定要监听的端口
-server.listen()#监听
+server.listen(5)#监听
 
 print("i wait connect")
-conn,addr = server.accept()#等待连接
-print(conn,addr)
-print("connect arrived")
 while True:
-    data = conn.recv(1024)
-    print("recive:",data)
-    conn.send(data.upper())
+    conn,addr = server.accept()#等待连接
+    #conn 就是客户端连接过来而在服务器为其生成的一个连接实例
+    print(conn,addr)
+    print("connect arrived")
+    count = 0
+    while True:
+        data = conn.recv(1024)
+        print("recive:",data)
+        if not data:
+            print("client has lost...")
+            break
+        conn.send(data.upper())
+        count +=1
+        if count > 10:break
 
-server.close()
+    server.close()
