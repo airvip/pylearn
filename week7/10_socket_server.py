7c64c47e7c5646d27657acf80582715572b53570
airvip <sdqhwzb@163.com>
2017-08-21 16:28:07 +0800

    socket

--- week7/10_socket_server.py
+++ week7/10_socket_server.py
@@ -10,12 +10,15 @@ server.bind(('localhost',6969))#绑定要监听的端口
 server.listen()#监听
 
 print("i wait connect")
-conn,addr = server.accept()#等待连接
-print(conn,addr)
-print("connect arrived")
 while True:
-    data = conn.recv(1024)
-    print("recive:",data)
-    conn.send(data.upper())
+    conn,addr = server.accept()#等待连接
+    print(conn,addr)
+    print("connect arrived")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print("recive:",data)
+        conn.send(data.upper())
 
 server.close()
