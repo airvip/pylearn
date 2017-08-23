b2dcd15eaf345b4c24ff43fcdabcf2f8b657fe67
airvip <sdqhwzb@163.com>
2017-08-23 18:14:09 +0800

    ThreadingTCPServer

---
+++ week8/socketserverbase_nult.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2017/8/23 17:40.
+
+import socketserver
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+
+        while True:
+            try:
+                self.data = self.request.recv(1024).strip()
+                print("{} wrote:".format(self.client_address[0]))
+                print(self.data.decode('utf-8'))
+                # if not self.data:#client break
+                #     print(self.client_address,"break")
+                #     break
+                self.request.send(self.data.upper())
+            except ConnectionResetError as e:
+                print("err:",e)
+                break
+
+if __name__ == "__main__":
+    HOST,PORT = "localhost",6969
+    #create the server,binding to localhost on port 9966
+    server = socketserver.ThreadingTCPServer((HOST,PORT),MyTCPHandler)
+    server.serve_forever()
+
