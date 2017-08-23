87d5f209caa6b2231a8d6a90df09d3c38ceb9d37
airvip <sdqhwzb@163.com>
2017-08-23 17:54:37 +0800

    My TCPsocket

---
+++ week8/socketserverbase.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2017/8/23 17:40.
+
+import socketserver
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+
+        while True:
+            self.data = self.request.recv(1024).strip()
+            print("{} wrote:".format(self.client_address[0]))
+            print(self.data)
+            if not self.data:#client break
+                print(self.client_address,"break")
+                break
+            self.request.send(self.data.upper())
+
+if __name__ == "__main__":
+    HOST,PORT = "localhost",6969
+    #create the server,binding to localhost on port 9966
+    server = socketserver.TCPServer((HOST,PORT),MyTCPHandler)
+    server.serve_forever()
+
