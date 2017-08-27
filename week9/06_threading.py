6f2b0b019e04019f6c328f564627453cb9b5ba53
airvip <sdqhwzb@163.com>
2017-08-27 23:14:38 +0800

    add thread6

--- week9/06_threading.py
+++ week9/06_threading.py
@@ -6,18 +6,19 @@ import threading
 import time
 
 def run(n):
-    print("task done",n,threading.current_thread())
+    print("task done",n)
     time.sleep(2)
 
 start_time = time.time()
 t_objs = []
 for i in range(50):
     t = threading.Thread(target=run,args=("t-%s"%i,))
+    t.setDaemon(True)#把当前线程设置成守护线程
     t.start()
     t_objs.append(t)
 
-for t in t_objs:
-    t.join()
+# for t in t_objs:
+#     t.join()
 
 print("all threads has finished".center(50,"*"),threading.current_thread(),threading.active_count())
 print("cost:",time.time() - start_time)
