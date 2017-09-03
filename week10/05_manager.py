12398deb0b8a5c996484a4ae93e6b4fe8fcc9666
airvip <sdqhwzb@163.com>
2017-09-03 22:58:30 +0800

    add multiprocessing manage

--- week10/05_manager.py
+++ week10/05_manager.py
@@ -3,12 +3,13 @@
 # Created by airvip on 2017/9/2 2:23.
 
 from multiprocessing import Process,Pipe,Manager
+import os
 
 def f(d,l):
     d[1] = '1'
     d['2'] = 2
     d[0.25] = None
-    l.append(l)
+    l.append(os.getpid())
     print(l)
 
 if __name__ == "__main__":
@@ -21,7 +22,7 @@ if __name__ == "__main__":
             p = Process(target=f,args=(d,l))
             p.start()
             p_list.append(p)
-        for res in p_list:
+        for res in p_list:#wait result
             res.join()
 
         print(d)
