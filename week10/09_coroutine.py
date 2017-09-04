7c634930d27e2c0ee0c0707d7c1b44938d6f532a
airvip <sdqhwzb@163.com>
2017-09-04 22:27:15 +0800

    coroutine

---
+++ week10/09_coroutine.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2017/9/4 21:55.
+
+import time
+import queue
+
+def home():
+    print("in func1")
+    time.sleep(5)
+    print("home exec done")
+
+def bbs():
+    print("in func2")
+
+def login():
+    print("in func3")
