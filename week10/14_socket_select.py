41bf82fe874003deb5b1434d1261838ae20a8bef
airvip <sdqhwzb@163.com>
2017-09-09 15:05:45 +0800

    add .

--- week10/14_socket_select.py
+++ week10/14_socket_select.py
@@ -20,6 +20,7 @@ server.listen(5)
 
 server.setblocking(0)#设置不阻塞
 
+msg_dic = {}
 inputs = [server,]
 # inputs = [server,conn]
 outputs = []
@@ -32,8 +33,26 @@ while True:
             conn,addr = server.accept()
             print("来了一个新链接",addr)
             inputs.append(conn)#因为新建立的连接还没有发数据过来，现在接数据的程序就报错
+            #所以要想实现这个客户端发数据来时server端能知道，就需要让select监听这个conn
+            msg_dic[conn] = queue.Queue()#初始化一个队列，后面存返回给这个客户端的数据
         else:
             data = r.recv(1024)
             print("收到数据",data)
-            r.send(data)
-            print("send done...")
+            msg_dic[r].put(data)
+
+            outputs.append(r)#放入返回的连接队列里
+            # r.send(data)
+            # print("send done...")
+
+    for w in writeable:#要返回给客户端的连接列表
+        data_to_client = msg_dic[w].get()
+        w.send(data_to_client)#返回给客户端源数据
+
+        outputs.remove(w)#确保下次循环的时候writeable,不返回已经处理完的数据
+
+    for e in exceptional:
+        if e in outputs:
+            outputs.reverse(e)
+        inputs.reverse(e)
+
+        del msg_dic[e]
