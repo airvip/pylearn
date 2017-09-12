146d7e884c665c5b77643080d26bd0c6787c9740
airvip <sdqhwzb@163.com>
2017-09-12 23:38:42 +0800

    add sqlalchemy

--- week13/goServer/models/models.py
+++ week13/goServer/models/models.py
@@ -2,4 +2,12 @@
 # -*- coding:utf-8 -*-
 # Created by airvip on 2017/9/10 12:38.
 
+from sqlalchemy import Table,Column,Integer,String,DATE,ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
 
