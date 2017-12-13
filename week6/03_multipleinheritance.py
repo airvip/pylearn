5c817d19df00d02bd3a618fc3cdf2342be9ac887
airvip <sdqhwzb@163.com>
2017-12-13 16:08:21 +0800

    继承搜索算法

--- week6/03_multipleinheritance.py
+++ week6/03_multipleinheritance.py
@@ -4,23 +4,24 @@
 
 #Python2经典类(继承深度优先继承搜索法) 新式类（继承广度优先继承搜索法）
 #Python3继承广度优先继承搜索法
-class A:
+class A(object):
     def __init__(self):
         print('A')
 
 class B(A):
-    pass
-    # def __init__(self):
-    #     print('B')
+    # pass
+    def __init__(self):
+        print('B')
 
 class C(A):
     pass
-    # def __init__(self):
-    #     print('C')
+    def __init__(self):
+        print('C')
 
 class D(B,C):
     pass
-    # def __init__(self):
-    #     print('C')
-
 D()
+
+class E(C,B):
+    pass
+E()
