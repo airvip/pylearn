7d17ae6d4f2d08392d7e3c073d5b0d5861294e9d
airvip <sdqhwzb@163.com>
2018-02-01 11:40:15 +0800

    在Linux中创建进程

--- process_threading/linux_fork.py
+++ process_threading/linux_fork.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-# Created by airvip on 2018/1/25 17:31.
+# only in Unix/Linux/Mac
 
 import os
 
-print("Process %s running..."% os.getpid())
+print("当前的进程 ID 为 ：%s " % os.getpid()) # os.getpid()返回的是进程的id
+# PID : 一个非负整型表示的唯一进程ID
+PID = os.fork() # 创建子进程，并返回进程的id，父进程返回的是子进程的id，子进程返回的是0
 
-pid = os.fork()
-print(pid)
-
-# if pid == 0:
-#     print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
-# else:
-#     print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
+if 0 == PID:
+    print('我是子进程，ID 为 (%s)，父进程 ID 为 (%s)' % (os.getpid(), os.getppid()))
+else:
+    print('我是父进程，ID 为 (%s)，创建了一个 ID 为 (%s) 的子进程' % (os.getpid(), PID))
