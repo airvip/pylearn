53ac341ab913fd6f0f6af7865428a984a3580672
airvip <sdqhwzb@163.com>
2018-03-22 10:52:07 +0800

    基础except02

--- error/error03.py
+++ error/error03.py
@@ -4,14 +4,13 @@
 
 def my_divide(x, y):
     try:
-        try:
-            res = x / y
-            return res
-        except NameError:
-            print('error: Name error')
-        except ImportError:
-            print('error: Import error')
-    except :
-        print('error: error undefined')
+        res = x / y
+        return res
+    except ZeroDivisionError:
+        print('error: division by zero!')
+    except TypeError:
+        print('error: Type error')
+    except ImportError:
+        print('error: Import error')
 
-my_divide(4,'4')
+my_divide(4,'4') # 调用
