2f93107a97369d717ddad1855dcbe676b474be9f
airvip <sdqhwzb@163.com>
2018-04-18 17:25:04 +0800

    print err

---
+++ error/print_err01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2018/4/18 17:06.
+
+def divi(n,m):
+    if int(m) == 0 :
+        return False
+    print('debug')
+    return n / m
+
+def main():
+    divi(20,0)
+
+main()
