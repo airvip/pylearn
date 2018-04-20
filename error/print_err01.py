245a4b97e421de3dd1209edd432b7c6d367ee91d
airvip <sdqhwzb@163.com>
2018-04-20 16:20:01 +0800

    print err

--- error/print_err01.py
+++ error/print_err01.py
@@ -2,13 +2,11 @@
 # -*- coding:utf-8 -*-
 # Created by airvip on 2018/4/18 17:06.
 
-def divi(n,m):
-    if int(m) == 0 :
-        return False
+def myadd(n,m):
     print('debug')
-    return n / m
+    return n + m
 
 def main():
-    divi(20,0)
+    myadd(20,'0')
 
 main()
