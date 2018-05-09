8dc5276fb83ee953a91f919836b037906801d042
airvip <sdqhwzb@163.com>
2018-05-09 15:00:19 +0800

    show image

---
+++ pillow/openimg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2018/5/9 13:49.
+
+
+from PIL import Image
+
+img = Image.open('hzw.png')
+img.show()
+print(img.info)
+print(img.size)
+print(img.mode)
+print(img.format)
