40003b6362685c893891a27149ef8a4d795b1a0e
airvip <sdqhwzb@163.com>
2018-05-29 10:42:15 +0800

    add csv

--- package3th/csv01.py
+++ package3th/csv01.py
@@ -17,8 +17,28 @@ for val in datalist:
 
 data.append([4, '木子', 18, 100])
 data.insert(0, [0, '维奇', 23, 100])
-print(data)
+
+data.pop()
+data.lpop()
+del data[0:2]
+
+data.append_col(['男'], header='性别')
+data.insert_col(2, ['中国'], header='国籍')
+
+del data['性别']
+
+
+
+# print(data)
+
+# print('JSON',data.export('json'))
+# print('JSON',data.json)
+# print('YAML',data.export('yaml'))
+# print('YAML',data.yaml)
+# print('CSV',data.export('csv'))
+# print('CSV',data.csv)
+
 
 # 导出 xls
-# with open('dataxls.xls', 'wb') as f:
-#     f.write(data.xls)
+with open('dataxls.xls', 'wb') as f:
+    f.write(data.xls)
