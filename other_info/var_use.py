417f1d41b5eeb793b82a9ae17323394533bf3973
airvip <sdqhwzb@163.com>
2018-06-21 16:56:51 +0800

    use

--- other_info/var_use.py
+++ other_info/var_use.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+list1 = [1, 2, 3]
 print(vars())
+print(vars()['list1'])
 
 
-class Foo (object):
-    nam = 'airvip'
+class Foo(object):
+    name = 'airvip'
+
+
+print(Foo)
+foo = Foo
+print(vars(foo))
+print(vars(foo)['name'])
+
+
+class People(object):
+
+    age = 17
+
+    def __init__(self, name):
+        name = name
+
+
+print(People)
+p = People
+print(vars(p))
+
 
