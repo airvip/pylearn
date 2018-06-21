01c481afd4e4186eccae3e463be53bb61f670576
airvip <sdqhwzb@163.com>
2018-06-21 17:15:03 +0800

    eval_use

---
+++ other_info/eval_use.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+name = 'airvip'
+
+print(eval('name'))
+# 直接打印变量信息 airvip
+
+
+list1 = eval("[[1, 2, 3],[1, 2, 3]]")
+print(list1)
+print(type(list1))
+
+
+print(eval("1,"))
+print(eval("{'name':'airvip'}"))
