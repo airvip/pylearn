6a85e9a59ca13b32c5c9bf0f9d6629a067ac89e1
airvip <sdqhwzb@163.com>
2018-07-07 15:12:42 +0800

    mongo创建数据库

--- sql/pymongo01.py
+++ sql/pymongo01.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-# Created by airvip on 2018/7/6 17:57.
 
 import pymongo
 
+'''
+创建数据库就要创建集合并且插入文档
+'''
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017")
+testdb = myclient["test"]
+print(myclient.list_database_names())
+
+coll = testdb['user']
+print(testdb.collection_names())
+
+info = {"name": "阿尔维奇", "age":"25"}
+doc = coll.insert_one(info)
+print(doc)
+print(doc.inserted_id)
+
+print(myclient.list_database_names())
+print(testdb.collection_names())
