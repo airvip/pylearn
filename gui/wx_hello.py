1fa0ef37cd35a1ec2a8bd682ae40e579e66b3cf5
airvip <sdqhwzb@163.com>
2018-07-09 11:52:38 +0800

    add wxpython

--- gui/wx_hello.py
+++ gui/wx_hello.py
@@ -5,19 +5,27 @@ import wx
 
 
 class Mywin(wx.Frame):
-    def __init__(self, parent, title="",size=(600, 400)):
+    def __init__(self, parent, title="",size=(300, 200)):
         super(Mywin, self).__init__(parent=parent, title=title, size=size)
-        self.panel = wx.Panel(self, -1)
+        self.SetMaxSize(wx.Size(300, 200))
+        self.init_ui()
         self.Show()
-        self.create_widgets()
 
-    def create_widgets(self):
-        self.label = wx.StaticText(self.panel, -1, label="Hello, world!", size=(600, 30), style=wx.ALIGN_CENTRE)
+    def init_ui(self):
+        panel = wx.Panel(self)
 
-        self.quit_btn = wx.Button(self.panel, -1, label='quit', pos=(30,30))
-        self.quit_btn.SetForegroundColour('red')
-        self.Bind(wx.EVT_BUTTON, handler=self.close_now, source=self.quit_btn)
+        # 创建一个垂直方向的BoxSizer
+        vbox = wx.BoxSizer(orient=wx.VERTICAL)
 
+        label = wx.StaticText(panel, -1, label="Hello, world!")
+        vbox.Add(label, flag=wx.TOP|wx.ALIGN_CENTER_HORIZONTAL)
+
+        quit_btn = wx.Button(panel, -1, label='quit')
+        quit_btn.SetForegroundColour('#FF0000')
+        self.Bind(wx.EVT_BUTTON, handler=self.close_now, source=quit_btn)
+        vbox.Add(quit_btn, flag=wx.ALIGN_CENTER_HORIZONTAL)
+
+        panel.SetSizer(vbox)
 
     def close_now(self, event):
         self.Close(True)
@@ -26,10 +34,3 @@ app = wx.App(False)
 win = Mywin(None, title='hi')
 app.MainLoop()
 
-
-# app = wx.App(False) # 实例化应用对象
-# win = wx.Frame(None, title='hi', size=(200, 100)) # 创建窗体
-# panel = wx.Panel(win)
-# wx.StaticText(parent=panel, label="Hello, world!")
-# win.Show()
-# app.MainLoop() # 运行程序
