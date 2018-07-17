a136485e8dd711f4fd8f392efaa76f96e8bd7865
airvip <sdqhwzb@163.com>
2018-07-17 11:32:42 +0800

    add all

---
+++ async_sync/nul_thread.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import requests
+import threading
+import time
+
+def blocking_way():
+    s = requests.get('https://www.baidu.com/')
+    print(s)
+
+
+
+if __name__ == "__main__":
+
+    start_time = time.time()
+    for i in range(10):
+        i = threading.Thread(target=blocking_way)
+        i.start()
+
+    end_time = time.time()
+    print('多进程并行调用耗时 %s 秒' % (end_time-start_time))
+
