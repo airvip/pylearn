1cd804686210626170cbe06dde4245cf1ecdc5fe
airvip <sdqhwzb@163.com>
2018-07-26 17:20:38 +0800

    add all yield

---
+++ async_sync/yield_producer_consumer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import time
+
+def consumer():
+    print("老板上包子")
+    bao = ''
+    while True:
+        baozi = yield bao
+        print("[消费者]-吃 %s"%(baozi))
+        time.sleep(0.1)
+        bao = "消费者说 {} 吃完了".format(baozi)
+
+def producer(c):
+    c.send(None)
+    n = 0
+    while n < 10:
+        n += 1
+        print("[生产者]-生产第 %s 个包子"%n)
+        bao = c.send("包子 {}".format(n))
+        print(bao)
+    c.close()
+
+producer(consumer())
+
