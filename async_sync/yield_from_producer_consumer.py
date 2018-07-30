5c9b4a8f1b38e38bb3aa973cd016136ced903ad9
airvip <sdqhwzb@163.com>
2018-07-30 17:26:18 +0800

    yeild

--- async_sync/yield_from_producer_consumer.py
+++ async_sync/yield_from_producer_consumer.py
@@ -3,17 +3,33 @@
 
 import time
 
+
 def eat():
+    print("老板上包子")
     bao = ''
     while True:
         baozi = yield bao
-        print("[消费者]-吃 %s"%(baozi))
+        print("[消费者]-吃 %s" % (baozi))
         time.sleep(0.1)
 
-
 def consumer():
-    print("老板上包子")
-    yield from eat()
+    geat = eat()
+    bao = None
+    res = geat.send(bao)
+    while True:
+        try:
+            bao = yield res
+            res = geat.send(bao)
+        except StopIteration:
+            break
+        except Exception as err:
+            try:
+                res = geat.throw(err)
+            except StopIteration:
+                break
+
+
+
 
 def producer(c):
     c.send(None)
