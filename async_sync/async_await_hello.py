0547e89c74046a47da86fe801d9e0ab8fed9a263
airvip <sdqhwzb@163.com>
2018-08-02 18:21:23 +0800

    async与await加持

---
+++ async_sync/async_await_hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+import asyncio
+
+async def hello(t):
+    print('Start Hello %s' %t)
+    await asyncio.sleep(t) #  表示不同时间的IO操作
+    print('End Hello %s' %t)
+
+# 获取 EventLoop
+loop = asyncio.get_event_loop()
+tasks = [hello(3), hello(2)]
+# 执行coroutine
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
