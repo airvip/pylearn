cb5750ccb497138451a7c6f84dc115cc66678021
airvip <sdqhwzb@163.com>
2018-08-03 19:00:21 +0800

    aiohttp

--- async_sync/aiohttp_c2.py
+++ async_sync/aiohttp_c2.py
@@ -6,13 +6,15 @@ import asyncio
 
 async def fetch(session, url):
     async with session.get(url) as response:
-        return await response.text()
+        return await response.text(encoding='UTF-8')
 
 async def main():
     async with aiohttp.ClientSession() as session:
         html = await fetch(session, 'http://www.baidu.com')
-        print(html)
+        print(len(html))
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    tasks = [ main() for i in range(10) ]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
