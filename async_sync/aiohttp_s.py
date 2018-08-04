fa7ed5a65a49e13bec3ed9b4bedcdf1fce2791a3
airvip <sdqhwzb@163.com>
2018-08-04 17:44:48 +0800

    add aiphttp server

--- async_sync/aiohttp_s.py
+++ async_sync/aiohttp_s.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-
-import asyncio
-
 from aiohttp import web
 
 async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
+    name = request.match_info.get('name', "Python")
     text = "Hello, " + name
     return web.Response(text=text)
 
@@ -15,5 +12,5 @@ app = web.Application()
 app.add_routes([web.get('/', handle),
                 web.get('/{name}', handle)])
 
-web.run_app(app)
+web.run_app(app, host='127.0.0.1', port=8888, shutdown_timeout=5)
 
