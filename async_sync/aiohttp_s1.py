298edeec6e808b5ddaf379ed46953775897fdf80
airvip <sdqhwzb@163.com>
2018-08-06 15:18:01 +0800

    add all aiohttp server1

--- async_sync/aiohttp_s1.py
+++ async_sync/aiohttp_s1.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-import asyncio
 from aiohttp import web
 
 routes = web.RouteTableDef()
 
 @routes.get('/')
+@routes.get('/{name}')
 async def hello(request):
     name = request.match_info.get('name', "Python")
     text = "Hello, " + name
     return web.Response(text=text)
 
 
-async def main(loop):
-    app = web.Application(loop=loop)
-    app.add_routes(routes)
-    resp = await loop.create_server(app.make_handler(), host='127.0.0.1', port=8888)
-    return resp
-
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(loop))
-    loop.run_forever()
+    app = web.Application()
+    app.add_routes(routes)
+    web.run_app(app, host='127.0.0.1', port=8888)
 
