a95ac013354d2ccbea5bc7ef4b1dae94de613046
airvip <sdqhwzb@163.com>
2018-08-17 16:58:35 +0800

    selenium

---
+++ spider/l_selenium.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+from selenium import webdriver
+
+
+browser = webdriver.Chrome()
+browser.get("https://www.baidu.com")
+print(browser.page_source)
+# browser.close()
