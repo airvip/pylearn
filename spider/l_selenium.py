38df5bdc7491e89f918013a9472294e1cef2c51b
airvip <sdqhwzb@163.com>
2018-08-17 18:07:53 +0800

    find ele

--- spider/l_selenium.py
+++ spider/l_selenium.py
@@ -7,5 +7,17 @@ from selenium import webdriver
 
 browser = webdriver.Chrome()
 browser.get("https://www.baidu.com")
-print(browser.page_source)
+# print(browser.page_source)
+# browser.close()
+
+
+browser.find_elements_by_class_name()
+
+head1 = browser.find_element_by_id("head")  # id 为 head
+head2 = browser.find_element_by_css_selector("#head")  # css 选择器
+head3 = browser.find_element_by_xpath('//div[@id="head"]')  # xpath 选择器
+print(head1)
+print(head2)
+print(head3)
+
 browser.close()
