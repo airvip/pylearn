baa5346a295d45bdf15d2dec13ca20853a8aacc6
airvip <sdqhwzb@163.com>
2018-08-29 18:13:26 +0800

    对数函数

---
+++ test/time_complex02.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2018/8/29 17:43.
+
+def use_t(n):
+    i = 1
+    while i < n:
+        print('走路中')
+        print('走路中')
+        print('到记录点一次')
+        i *= 2
+
+
+
+use_t(10)
