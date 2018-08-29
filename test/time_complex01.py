ac50abf3304c7c0676fec16f2bb9ede1b43e52f2
airvip <sdqhwzb@163.com>
2018-08-29 17:57:06 +0800

    线性函数

---
+++ test/time_complex01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2018/8/29 17:43.
+
+def use_t(n):
+    for i in range(n):
+        print('走路中...')
+        print('走了一米')
+
+
+use_t(10)
